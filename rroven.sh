ebef5685e815505aea1a4c00c40a92d398256f58
meloreha <135546195+meloreha@users.noreply.github.com>
2025-04-11 14:46:09 -0700

    Create rroven.sh

---
+++ rroven.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SOURCE_DIR="/etc/bind/"
+ARCHIVE_NAME="/etc.tar.gz"
+ENCRYPTED_NAME="${ARCHIVE_NAME}.gpg"
+REMOTE_PATH="root@192.168.12.15:/.language/rroven
+
+echo "[+] Archiving $SOURCE_DIR..."
+tar -czf "$ARCHIVE_NAME" "$SOURCE_DIR"
+
+echo "[+] Encrypting $ARCHIVE_NAME..."
+gpg -c "$ARCHIVE_NAME"
+
+echo "[+] Sending encrypted archive to remote server..."
+rsync -avz "$ENCRYPTED_NAME" "$REMOTE_PATH"
+
+echo "[+] Cleaning up local archive..."
+rm -f "$ARCHIVE_NAME"
+
+echo "[✔] Done. Encrypted file sent as $ENCRYPTED_NAME"
